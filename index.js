cbfda55494419d2d6d9527401b9d4dbd9f12c629
Sara BEGACHE <sara.begache@gmail.com>
2023-07-23 11:54:05 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < namesArray.length; i++) {
+      let message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+
+  function countDown(startingNumber) {
+    let count = startingNumber;
+  
+    while (count >= 0) {
+      console.log(count);
+      count--;
+    }
+  }
+  
+  
